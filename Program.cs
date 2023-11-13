29c076f204c912d302c67a52770fed20ec8a3f86
dayat30 <150218214+dayat30@users.noreply.github.com>
2023-11-13 11:48:02 +0700

    Update

--- Program.cs
+++ Program.cs
@@ -1,12 +1,1 @@
-﻿using System;
 
-namespace ConsoleApp3
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
